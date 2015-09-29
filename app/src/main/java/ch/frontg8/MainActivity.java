d660bf39dc2a25afd32fd61523a5998aa7743348
Ueli Bosshard <ueli@lilo.ch>
2015-09-29 15:47:11 +0000

    menu-entries added

--- app/src/main/java/ch/frontg8/MainActivity.java
+++ app/src/main/java/ch/frontg8/MainActivity.java
@@ -1,5 +1,7 @@
 package ch.frontg8;
 
+import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -22,16 +24,29 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Handle item selection
+        switch (id) {
+            case R.id.contacts:
+                Intent intent = new Intent(this, ContactActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.conversation:
+                Intent intent2 = new Intent(this, ConversationActivity.class);
+                startActivity(intent2);
+                return true;
+            case R.id.action_settings:
+                //startActivity(new Intent(Settings.ACTION_SETTINGS));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
 
-        return super.onOptionsItemSelected(item);
+
+        //noinspection SimplifiableIfStatement
+        //if (id == R.id.action_settings) {
+        //    return true;
+        //}
+        //return super.onOptionsItemSelected(item);
     }
 }
