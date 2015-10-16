40fcb58b3683e62a362706e47c7302aed5081c4f
Ueli Bosshard <ueli@lilo.ch>
2015-10-16 16:44:42 +0000

    new settings

--- app/src/main/java/ch/frontg8/view/SettingsActivity.java
+++ app/src/main/java/ch/frontg8/view/SettingsActivity.java
@@ -3,6 +3,7 @@ package ch.frontg8.view;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
 
@@ -25,6 +26,16 @@ public class SettingsActivity extends AppCompatActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
+        }
+        public void onResume() {
+            super.onResume();
+
+            EditTextPreference preference_hostname = (EditTextPreference) this.getPreferenceManager().findPreference("edittext_preference_hostname");
+            EditTextPreference preference_port = (EditTextPreference) this.getPreferenceManager().findPreference("edittext_preference_port");
+
+            preference_hostname.setSummary(preference_hostname.getText().toString());
+            preference_port.setSummary(preference_port.getText().toString());
+
         }
     }
 
