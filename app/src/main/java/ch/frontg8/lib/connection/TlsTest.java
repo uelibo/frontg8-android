0b43df72e8981b1eba0a1e5a38cecc681141cebd
Ueli Bosshard <ueli@lilo.ch>
2015-11-06 11:32:09 +0100

    helper methods for Protobuf added

--- app/src/main/java/ch/frontg8/lib/connection/TlsTest.java
+++ app/src/main/java/ch/frontg8/lib/connection/TlsTest.java
@@ -2,9 +2,9 @@ package ch.frontg8.lib.connection;
 
 import android.app.Activity;
 
-import com.google.protobuf.ByteString;
 import com.google.protobuf.InvalidProtocolBufferException;
 
+import ch.frontg8.lib.crypto.LibCrypto;
 import ch.frontg8.lib.message.MessageHelper;
 import ch.frontg8.lib.protobuf.Frontg8Client;
 
@@ -21,32 +21,29 @@ public class TlsTest {
         TlsClient tlsClient = new TlsClient("redmine.frontg8.ch", 40001, Log, context);
         tlsClient.connect();
 
-        byte[] packet;
-        packet = new byte[] {
-            (byte)0x00, 0x14, 0x01, 0x00, 0x0a, 0x12, 0x30, 0x31,
-            (byte)0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39,
-            (byte)0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48
-            };
+        Frontg8Client.Data data = MessageHelper.buildDataMessage("frontg8 Test Message", "0", 0);
 
-        Log.TRACE("---\n" + MessageHelper.byteArrayAsHexString(packet));
+        // TODO: Encryption of data
+        try {
+            LibCrypto.generateNewKeys(context);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        Frontg8Client.MessageRequest request = Frontg8Client.MessageRequest.newBuilder().setHash(ByteString.copyFromUtf8("0")).build();
-        Frontg8Client.Data data = Frontg8Client.Data.newBuilder().setMessageData(ByteString.copyFromUtf8("Hallo")).setSessionId(ByteString.copyFromUtf8("0")).setTimestamp(0).build();
-        Frontg8Client.Encrypted encr = Frontg8Client.Encrypted.newBuilder().setEncryptedData(ByteString.copyFromUtf8("Hallo")).build();
+        Frontg8Client.Encrypted encr = MessageHelper.buildEncryptedMessage(data.toByteString());
 
-        Log.TRACE("---\n" + MessageHelper.byteArrayAsHexString(request.toByteArray()));
         Log.TRACE("---\n" + MessageHelper.byteArrayAsHexString(encr.toByteArray()));
-        Log.TRACE("---\n" + MessageHelper.byteArrayAsHexString(data.toByteArray()));
+        Log.TRACE("---\n" + MessageHelper.byteArrayAsHexString(MessageHelper.addMessageHeader(encr.toByteArray())));
         Log.TRACE("---\n");
 
-        tlsClient.sendBytes(packet);
+        // SEND
+        tlsClient.sendBytes(MessageHelper.addMessageHeader(encr.toByteArray()));
 
         byte[] recv1 = tlsClient.getBytes(4);
         Log.TRACE("---\n" + MessageHelper.byteArrayAsHexString(recv1));
 
         int length = recv1[0] * 256 + recv1[1] ;
-        int proto = recv1[2];
-        Log.TRACE("---\n" + "Length: " + length + " Proto: " + proto);
+        //int proto = recv1[2];
 
         byte[] recv2 = tlsClient.getBytes(length);
         Log.TRACE("---\n" + MessageHelper.byteArrayAsHexString(recv2));
@@ -55,8 +52,8 @@ public class TlsTest {
             Frontg8Client.Notification notification = Frontg8Client.Notification.parseFrom(recv2);
             Frontg8Client.Encrypted encrypted = notification.getBundle(0);
             Frontg8Client.Data message= Frontg8Client.Data.parseFrom(encrypted.getEncryptedData());
-            Log.TRACE("\nMessage: " + message.getMessageData().toString());
-        } catch (InvalidProtocolBufferException e) {
+            Log.TRACE("\nMessage: " + message.getMessageData().toStringUtf8());
+            } catch (InvalidProtocolBufferException e) {
             Log.TRACE(e.getMessage());
             e.printStackTrace();
         }
--- app/src/main/java/ch/frontg8/lib/message/MessageHelper.java
+++ app/src/main/java/ch/frontg8/lib/message/MessageHelper.java
@@ -1,6 +1,10 @@
 package ch.frontg8.lib.message;
 
+import com.google.protobuf.ByteString;
+
 import java.math.BigInteger;
+
+import ch.frontg8.lib.protobuf.Frontg8Client;
 
 public class MessageHelper {
 
@@ -12,8 +16,53 @@ public class MessageHelper {
         return(sb.toString());
     }
 
-    public static byte[] sizeToByte(int size){
+    public static Frontg8Client.MessageRequest buildMessageRequestMessage(String hash) {
+        return Frontg8Client.MessageRequest
+                .newBuilder()
+                .setHash(ByteString.copyFromUtf8(hash))
+                .build();
+    }
+
+    public static Frontg8Client.Data buildDataMessage(String message, String sessionId, int timestamp) {
+        return Frontg8Client.Data
+                .newBuilder()
+                .setMessageData(ByteString.copyFromUtf8(message))
+                .setSessionId(ByteString.copyFromUtf8(sessionId))
+                .setTimestamp(0)
+                .build();
+    }
+
+    public static Frontg8Client.Encrypted buildEncryptedMessage(ByteString data)  {
+        return Frontg8Client.Encrypted
+                .newBuilder()
+                .setEncryptedData(data)
+                .build();
+    }
+
+    private static byte[] sizeToByte(int size){
         return BigInteger.valueOf(size).toByteArray();
+    }
+
+    public static byte[] addMessageHeader(byte[] message) {
+        byte[] header = new byte[4];
+        byte[] sizeArray = sizeToByte(message.length);
+
+        header[2] = (byte) MessageType.Encrypted;
+
+        if (sizeArray.length == 2) {
+            header[0] = sizeArray[1];
+            header[1] = sizeArray[0];
+        } else if (sizeArray.length == 1) {
+            header[1] = sizeArray[0];
+        } else {
+            // TODO: raise new Exception();
+        }
+
+        byte[] fullMessage = new byte[header.length + message.length];
+        System.arraycopy(header, 0, fullMessage, 0, header.length);
+        System.arraycopy(message, 0, fullMessage, header.length, message.length);
+
+        return fullMessage;
     }
 
 }
