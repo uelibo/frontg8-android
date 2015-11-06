cd9231eb4e3e204524f3fdb7e57433ce4da1e7ea
Stautob <stautob@stautob.ch>
2015-11-06 10:47:58 +0100

    Adapted ConnectionService to TlsClient
    Change-Id: Ib7b09edc143c387785559c2893fcbef2c95a0e07

--- app/src/main/java/ch/frontg8/lib/connection/ConnectionService.java
+++ app/src/main/java/ch/frontg8/lib/connection/ConnectionService.java
@@ -4,14 +4,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
-import android.util.Log;
 import android.widget.Toast;
-
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.Socket;
 
 public class ConnectionService extends Service {
     public ConnectionService() {
@@ -20,9 +13,28 @@ public class ConnectionService extends Service {
     public static final String SERVERIP = "server.frontg8.ch";
     public static final int SERVERPORT = 40001;
     private Logger logger = new Logger();
-    PrintWriter out;
-    Socket socket;
-    InetAddress serverAddr;
+    private TlsClient mTlsClient;
+    private final IBinder myBinder = new LocalBinder();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        System.out.println("I am in on create");
+
+        mTlsClient = new TlsClient(SERVERIP, SERVERPORT, logger, this);
+        mTlsClient.connect();
+    }
+
+//    @Override
+//    public int onStartCommand(Intent intent, int flags, int startId) {
+//        super.onStartCommand(intent, flags, startId);
+//        System.out.println("I am in on start");
+//
+////        //  Toast.makeText(this,"Service created ...", Toast.LENGTH_LONG).show();
+////        Runnable connect = new connectSocket();
+////        new Thread(connect).start();
+//        return START_STICKY;
+//    }
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -31,10 +43,6 @@ public class ConnectionService extends Service {
         return myBinder;
     }
 
-    private final IBinder myBinder = new LocalBinder();
-    //TCPClient mTcpClient = new TCPClient();
-    TlsClient mTlsClient = new TlsClient(SERVERIP,SERVERPORT,Log.d(),this);
-
     public class LocalBinder extends Binder {
         public ConnectionService getService() {
             System.out.println("I am in Localbinder ");
@@ -42,69 +50,29 @@ public class ConnectionService extends Service {
         }
     }
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        System.out.println("I am in on create");
-    }
-
     public void IsBoundable() {
         Toast.makeText(this, "I bind like butter", Toast.LENGTH_LONG).show();
     }
 
-    public void sendMessage(String message) {
-        if (out != null && !out.checkError()) {
-            System.out.println("in sendMessage" + message);
-            out.println(message);
-            out.flush();
+    public void sendBytes(byte[] packet) {
+        if (!mTlsClient.isConnected()) {
+            mTlsClient.connect();
         }
+        mTlsClient.sendBytes(packet);
     }
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        super.onStartCommand(intent, flags, startId);
-        System.out.println("I am in on start");
-        //  Toast.makeText(this,"Service created ...", Toast.LENGTH_LONG).show();
-        Runnable connect = new connectSocket();
-        new Thread(connect).start();
-        return START_STICKY;
-    }
-
-
-    class connectSocket implements Runnable {
-
-        @Override
-        public void run() {
-            try {
-                serverAddr = InetAddress.getByName(SERVERIP);
-                Log.e("TCP Client", "C: Connecting...");
-                //create a socket to make the connection with the server
-                socket = new Socket(serverAddr, SERVERPORT);
-                try {
-                    //send the message to the server
-                    out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-                    Log.e("TCP Client", "C: Sent.");
-                    Log.e("TCP Client", "C: Done.");
-                } catch (Exception e) {
-                    Log.e("TCP", "S: Error", e);
-                }
-            } catch (Exception e) {
-                Log.e("TCP", "C: Error", e);
-            }
+    public byte[] getBytes(int length) {
+        if (!mTlsClient.isConnected()) {
+            mTlsClient.connect();
         }
+        return mTlsClient.getBytes(length);
     }
-
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        try {
-            socket.close();
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        socket = null;
+        mTlsClient.close();
+        mTlsClient = null;
     }
 }
 
