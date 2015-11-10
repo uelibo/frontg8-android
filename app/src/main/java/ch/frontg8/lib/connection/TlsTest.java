22b4425a08056f5399748fde0a76862eb21d99fc
Ueli Bosshard <ueli@lilo.ch>
2015-11-10 16:52:30 +0100

    removed unused imports

--- app/src/main/java/ch/frontg8/lib/connection/TlsTest.java
+++ app/src/main/java/ch/frontg8/lib/connection/TlsTest.java
@@ -2,17 +2,12 @@ package ch.frontg8.lib.connection;
 
 import android.app.Activity;
 
-import com.google.protobuf.ByteString;
-
-import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
 import javax.net.ssl.SSLContext;
 
 import ch.frontg8.lib.config.LibConfig;
-import ch.frontg8.lib.crypto.KeyNotFoundException;
-import ch.frontg8.lib.crypto.LibCrypto;
 import ch.frontg8.lib.crypto.LibSSLContext;
 import ch.frontg8.lib.message.InvalidMessageException;
 import ch.frontg8.lib.message.MessageHelper;
