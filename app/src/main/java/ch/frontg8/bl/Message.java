49fdd16e22a9aaf67dc84516d8cadbf07d197e51
Ueli Bosshard <ueli@lilo.ch>
2015-12-04 11:44:15 +0100

    changed Message to Frontg8.Data container

--- app/src/main/java/ch/frontg8/bl/Message.java
+++ app/src/main/java/ch/frontg8/bl/Message.java
@@ -2,36 +2,40 @@ package ch.frontg8.bl;
 
 import java.io.Serializable;
 
+import ch.frontg8.lib.message.MessageHelper;
 import ch.frontg8.lib.protobuf.Frontg8Client;
 
 public class Message implements Serializable, Filtertext {
-    private String messagetext;
-    private String sessionID = "0";
-    private long timestamp = 0;
     private Frontg8Client.Encrypted encryptedMessage = null;
+    private Frontg8Client.Data dataMessage = null;
 
+    // Please use this constructor!
     public Message(Frontg8Client.Data dataMessage) { replaceMessage(dataMessage); }
 
     // Simple Constructor for Prototype
-    public Message(String messagetext) { this.messagetext = messagetext; }
+    @Deprecated
+    public Message(String messagetext) {
+        byte[] sessionId = new byte[]{};
+        int timestamp = 0;
+        this.dataMessage = MessageHelper.buildDataMessage(messagetext.getBytes(), sessionId, timestamp);
+    }
 
     // Constructor for encrypted Message
+    @Deprecated
     public Message(Frontg8Client.Encrypted encryptedMessage) {
-        this.messagetext = "encrypted Message";
+        this("encrypted Message");
         this.encryptedMessage = encryptedMessage;
     }
 
     public void replaceMessage(Frontg8Client.Data dataMessage) {
-        messagetext = dataMessage.getMessageData().toStringUtf8();
-        sessionID = dataMessage.getSessionId().toStringUtf8();
-        timestamp = dataMessage.getTimestamp();
+        this.dataMessage = dataMessage;
         encryptedMessage = null;
     }
 
-    public String getMessage() { return messagetext; }
-    public String getSessionID() { return sessionID; }
-    public long getTimestamp() { return timestamp; }
-    public String getFilterValue() { return this.messagetext; }
+    public String getMessage() { return dataMessage.getMessageData().toStringUtf8(); }
+    public String getSessionID() { return dataMessage.getSessionId().toStringUtf8(); }
+    public long getTimestamp() { return dataMessage.getTimestamp(); }
+    public String getFilterValue() { return getMessage(); }
     public Frontg8Client.Encrypted getEncryptedMessage() { return this.encryptedMessage; }
     public boolean isEncrypted() { return encryptedMessage != null; }
 
